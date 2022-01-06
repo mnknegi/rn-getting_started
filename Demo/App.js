6e06fdb918d016b678c431e287ea36ab1c29ff3e
imayanknegi29 <imayanknegi29@gmail.com>
2022-01-06 13:47:04 +0530

    Added Section List Demo

--- Demo/App.js
+++ Demo/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {View} from 'react-native';
 import FlatListExample from './src/FlatList';
+import SectionListExample from './src/SectionListBasics';
 
 const App = () => {
-  return <FlatListExample />;
+  // return <FlatListExample />;  // render a FlatList
+  return <SectionListExample />; // render a SectionList
 };
 
 export default App;
--- Demo/src/FlatList.js
+++ Demo/src/FlatList.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import {View, StyleSheet, FlatList, Text} from 'react-native';
+import {View, StyleSheet, FlatList, Text, Image} from 'react-native';
 
 const FlatListExample = () => {
   return (
     <View style={styles.container}>
       <FlatList
         data={[
-          'Spider-man',
-          'Superman',
-          'Batman',
-          'Doctor Strange',
-          'Venom',
-          'Xman',
-          'Iron-man',
+          {name: 'Spiderman', img: '../assets/spiderman'},
+          {name: 'Superman', img: '../assets/superman'},
+          {name: 'Batman', img: '../assets/batman'},
+          {name: 'Doctor Strange', img: '../assets/strange'},
+          {name: 'Venom', img: '../assets/venom'},
+          {name: 'Xman', img: '../assets/xman'},
+          {name: 'Ironman', img: '../assets/ironman'},
         ]}
-        renderItem={({item}) => <Text style={styles.cellItem}> {item} </Text>}
+        renderItem={({item}) => (
+          <View style={styles.superheroCell}>
+            <Image source={item.img} style={styles.superheroImage} />
+            <Text style={styles.superheroItem}> {item.name} </Text>
+          </View>
+        )}
       />
     </View>
   );
@@ -26,10 +31,25 @@ const styles = StyleSheet.create({
     paddingTop: 44,
     paddingBottom: 34,
   },
-  cellItem: {
+  superheroCell: {
+    // justifyContent: 'center',
     padding: 10,
-    height: 44,
-    fontSize: 20,
+    height: 60,
+    backgroundColor: 'lightgray',
+    borderColor: 'gray',
+    borderBottomWidth: 0.3,
+    flexDirection: 'row',
+  },
+  superheroItem: {
+    fontSize: 24,
+    backgroundColor: 'orange',
+  },
+  superheroImage: {
+    width: 40,
+    height: 40,
+    paddingTop: 5,
+    paddingBottom: 5,
+    backgroundColor: 'yellow',
   },
 });
 

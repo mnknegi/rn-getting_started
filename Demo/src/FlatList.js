76886cd9b568262b5fb3539c0e1fcf9215a229fd
imayanknegi29 <imayanknegi29@gmail.com>
2022-01-06 12:41:57 +0530

    WIP in FlatList. Will continue when complete working with images

--- Demo/src/FlatList.js
+++ Demo/src/FlatList.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import {View, StyleSheet, FlatList, Text} from 'react-native';
+import {View, StyleSheet, FlatList, Text, Image} from 'react-native';
 
 const FlatListExample = () => {
   return (
     <View style={styles.container}>
       <FlatList
         data={[
-          'Spider-man',
-          'Superman',
-          'Batman',
-          'Doctor Strange',
-          'Venom',
-          'Xman',
-          'Iron-man',
+          {name: 'Spiderman', img: '../assets/spiderman'},
+          {name: 'Superman', img: '../assets/superman'},
+          {name: 'Batman', img: '../assets/batman'},
+          {name: 'Doctor Strange', img: '../assets/strange'},
+          {name: 'Venom', img: '../assets/venom'},
+          {name: 'Xman', img: '../assets/xman'},
+          {name: 'Ironman', img: '../assets/ironman'},
         ]}
         renderItem={({item}) => (
-          <View style={styles.cellView}>
-            <Text style={styles.cellItem}> {item} </Text>
+          <View style={styles.superheroCell}>
+            <Image source={item.img} style={styles.superheroImage} />
+            <Text style={styles.superheroItem}> {item.name} </Text>
           </View>
         )}
       />
@@ -30,15 +31,25 @@ const styles = StyleSheet.create({
     paddingTop: 44,
     paddingBottom: 34,
   },
-  cellView: {
+  superheroCell: {
+    // justifyContent: 'center',
     padding: 10,
     height: 60,
     backgroundColor: 'lightgray',
     borderColor: 'gray',
     borderBottomWidth: 0.3,
+    flexDirection: 'row',
   },
-  cellItem: {
-    fontSize: 20,
+  superheroItem: {
+    fontSize: 24,
+    backgroundColor: 'orange',
+  },
+  superheroImage: {
+    width: 40,
+    height: 40,
+    paddingTop: 5,
+    paddingBottom: 5,
+    backgroundColor: 'yellow',
   },
 });
 

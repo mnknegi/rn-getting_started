72a0150d0680182da18fa0c770a2cc038002d2a8
imayanknegi29 <imayanknegi29@gmail.com>
2022-01-07 14:24:16 +0530

    Animal section complete

--- Demo/src/components/RenderItem.js
+++ Demo/src/components/RenderItem.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Image} from 'react-native';
 
 const RenderItem = props => {
   return (
     <View style={styles.itemView}>
-      <Text style={styles.cellItem}> {props.item} </Text>
+      <Image source={props.item.img} style={styles.cellImage} />
+      <Text style={styles.cellItem}> {props.item.item} </Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   itemView: {
-    height: 60,
+    height: 120,
     backgroundColor: 'smokewhite',
     borderColor: 'gray',
     borderBottomWidth: 0.3,
-    justifyContent: 'center',
+    flexDirection: 'row',
   },
   cellItem: {
     fontSize: 22,
     fontFamily: 'Optima',
+    alignSelf: 'center',
+  },
+  cellImage: {
+    height: 80,
+    width: 80,
+    margin: 10,
+    borderRadius: 5,
+    alignSelf: 'center',
   },
 });
 
--- Demo/src/screens/SectionListBasics.js
+++ Demo/src/screens/SectionListBasics.js
@@ -9,7 +9,19 @@ const SectionListExample = () => {
     <SafeAreaView style={styles.container}>
       <SectionList
         sections={[
-          {title: 'Animals', data: ['Cat', 'Dog', 'Tiger', 'Monkey', 'Lion']},
+          {
+            title: 'Animals',
+            data: [
+              {item: 'Cat', img: require('../../assets/animals/cat.jpeg')},
+              {item: 'Dog', img: require('../../assets/animals/dog.jpeg')},
+              {item: 'Tiger', img: require('../../assets/animals/tiger.jpeg')},
+              {
+                item: 'Monkey',
+                img: require('../../assets/animals/monkey.jpeg'),
+              },
+              {item: 'Lion', img: require('../../assets/animals/lion.jpeg')},
+            ],
+          },
           {
             title: 'Superheros',
             data: ['Spiderman', 'Batman', 'Superman', 'Xman', 'Ironman'],
@@ -18,7 +30,10 @@ const SectionListExample = () => {
             title: 'Vegis',
             data: ['Carrot', 'LadyFinger', 'Cabbage', 'Coliflower', 'Bringle'],
           },
-          {title: 'Fruits', data: ['Banana', 'Apple', 'Grapes', 'Water Melon']},
+          {
+            title: 'Fruits',
+            data: ['Banana', 'Apple', 'Grapes', 'Water Melon'],
+          },
         ]}
         renderItem={({item}) => <RenderItem item={item} />}
         renderSectionHeader={({section}) => <SectionHeader section={section} />}
@@ -38,6 +53,13 @@ const styles = StyleSheet.create({
     paddingTop: 5,
     paddingBottom: 5,
     backgroundColor: 'yellow',
+  },
+  superheroImage: {
+    height: 80,
+    width: 80,
+    paddingTop: 5,
+    paddingBottom: 5,
+    borderRadius: 5,
   },
 });
 
